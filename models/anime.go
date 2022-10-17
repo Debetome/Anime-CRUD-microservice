29eca3a88df6bd58f63aa167a55f3641eb40fdec
Debetome <magregory@tutanota.com>
2022-10-17 03:44:37 -0500

    Fixing not reading booleans bug

--- models/anime.go
+++ models/anime.go
@@ -7,10 +7,10 @@ import (
 )
 
 type Anime struct {	
-	ID 		  primitive.ObjectID  	`json:"_id,omitempty"       bson:"_id,omitempty"`
-	Emision   bool	  				`json:"emision,omitempty"  bson:"emision,omitempty"`
+	ID 		  primitive.ObjectID  	`json:"id,omitempty"       bson:"_id,omitempty"`
+	Emision   bool	  				`json:"emision"            bson:"emision"`
 	Name 	  string  				`json:"name,omitempty"     bson:"name,omitempty"`		
-	Watched   bool    				`json:"watched,omitempty"  bson:"wacthed,omitempty"`
+	Watched   bool    				`json:"watched"   		   bson:"wacthed"`
 	Seasons   int 	  				`json:"seasons,omitempty"  bson:"seasons,omitempty"`
 }
 
@@ -18,6 +18,11 @@ type Animes []*Anime
 
 func NewAnime(emision bool, name string, wacthed bool, seasons int) (a *Anime) {
 	return &Anime{ Emision: emision, Name: name, Watched: wacthed, Seasons: seasons }
+}
+
+func (p *Anime) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
 }
 
 func (p *Anime) FromJSON(r io.Reader) error {
--- models/message.go
+++ models/message.go
@@ -6,13 +6,8 @@ import (
 )
 
 type Message struct {
-	Msg 	string 	`json: "msg,omitempty"`
-	Status	string 	`json: "status,omitempty"`
-}
-
-func (p *Message) FromJSON(r io.Reader) error {
-	e := json.NewDecoder(r)
-	return e.Decode(p)
+	Msg 	string 	`json:"msg,omitempty"`
+	Status	string 	`json:"status,omitempty"`
 }
 
 func (p *Message) ToJSON(w io.Writer) error {
