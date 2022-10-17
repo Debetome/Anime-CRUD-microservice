381c6f5aca15be6834cb8072cd5bb00b243714fb
Debetome <magregory@tutanota.com>
2022-10-17 00:06:39 -0500

    Fixing not document bug

--- models/anime.go
+++ models/anime.go
@@ -7,16 +7,16 @@ import (
 )
 
 type Anime struct {	
-	ID 		  primitive.ObjectID  	`json: "id, omitempty"       bson: "_id, omitempty"`
-	Emision   bool	  				`json: "emision, omitempty"  bson: "emision, omitempty"`
-	Name 	  string  				`json: "name, omitempty"     bson: "name, omitempty"`		
-	Watched   bool    				`json: "watched, omitempty"  bson: "wacthed, omitempty"`
-	Seasons   int 	  				`json: "seasons, omitempty"  bson: "seasons, omitempty"`
+	ID 		  primitive.ObjectID  	`json:"_id,omitempty"       bson:"_id,omitempty"`
+	Emision   bool	  				`json:"emision,omitempty"  bson:"emision,omitempty"`
+	Name 	  string  				`json:"name,omitempty"     bson:"name,omitempty"`		
+	Watched   bool    				`json:"watched,omitempty"  bson:"wacthed,omitempty"`
+	Seasons   int 	  				`json:"seasons,omitempty"  bson:"seasons,omitempty"`
 }
 
 type Animes []*Anime
 
-func NewAnime(emission bool, name string, wacthed bool, seasons int) (a *Anime) {
+func NewAnime(emision bool, name string, wacthed bool, seasons int) (a *Anime) {
 	return &Anime{ Emision: emision, Name: name, Watched: wacthed, Seasons: seasons }
 }
 
