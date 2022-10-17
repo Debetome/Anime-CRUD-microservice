cb904a7a32ba3ab17e060efcc99e880946a01cfa
Debetome <magregory@tutanota.com>
2022-10-17 00:07:23 -0500

    Fixing not updating records bug

--- database/database.go
+++ database/database.go
@@ -71,7 +71,10 @@ func (self *Database) GetRecord(id string) (record *models.Anime, err error) {
 func (self *Database) AddRecord(anime *models.Anime) (err error) {
 	collection := self.client.Database("AnimesDB").Collection("animeList")
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	_, err = collection.InsertOne(ctx, anime)
+	_, err = collection.InsertOne(
+		ctx, 
+		bson.M{"emision": anime.Emision, "name": anime.Name, "watched": anime.Watched, "seasons": anime.Seasons},
+	)
 
 	if err != nil {
 		self.logger.Println(err)
@@ -82,8 +85,8 @@ func (self *Database) AddRecord(anime *models.Anime) (err error) {
 	return
 }
 
-func (self *Database) UpdateRecord(id string) (err error) {
-	anime, err := self.GetRecord(id)
+func (self *Database) UpdateRecord(id string, changed *models.Anime) (err error) {
+	_, err = self.GetRecord(id)
 	if err != nil {
 		self.logger.Println(err)
 		self.logger.Println("Unable to get record for updating")
@@ -101,7 +104,11 @@ func (self *Database) UpdateRecord(id string) (err error) {
 		return
 	}
 
-	_, err = collection.UpdateOne(ctx, bson.M{"_id": hexID}, bson.M{"$set": anime})
+	_, err = collection.UpdateOne(
+		ctx, 
+		bson.M{"_id": hexID}, 
+		bson.M{"$set": changed},
+	)
 
 	if err != nil {
 		self.logger.Println(err)
